c206b3d7b4488f30c0447167b3e8c0fe1a931ad9
dipak-trio-spire <dipak@trio-spire.com>
2025-06-05 12:06:01 +0530

    add new chages

--- src/app/components/Contact_form.js
+++ src/app/components/Contact_form.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import Fahne from "../../../public/images/Fahne.svg"; 
 import Key from "../../../public/images/KEYs.svg"; 
-import Tree from "../../../public/images/Tree.svg"; 
+// import Tree from "../../../public/images/Tree.svg"; 
 import airoplain from "../../../public/images/airoplain.svg";
 import heart from "../../../public/images/heart.svg";
 import home from "../../../public/images/home.svg";
