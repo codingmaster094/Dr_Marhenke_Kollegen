5113588d881bed7c8039b46bc24d6b6f31be6d07
dipak-trio-spire <dipak@trio-spire.com>
2025-07-09 10:54:54 +0530

    kontakt page chenges

--- src/app/components/ContactSection.js
+++ src/app/components/ContactSection.js
@@ -7,7 +7,7 @@ const ContactSection = () => {
       <div className="container">
         <div className="flex flex-col lg:flex-row gap-8 xl:gap-16 text-center xl:flex-nowrap flex-wrap justify-center">
           <div className="lg:w-[calc(50%-32px)] 2xl:w-[calc(33.33%-42.67px)] p-4 sm:p-6 sm:rounded-3xl bg-white shadow-custom_shdow2 hover:shadow-2xl transition-all">
-            <div className="scale-75 lg:scale-100 size-[100px] rounded-[10px] grid place-items-center bg-yellow mx-auto shadow-[0_4px_18px_0_rgba(0,0,0,.14)]">
+            {/* <div className="scale-75 lg:scale-100 size-[100px] rounded-[10px] grid place-items-center bg-yellow mx-auto shadow-[0_4px_18px_0_rgba(0,0,0,.14)]">
               <Image
                 role="img"
                 alt="men-icon"
@@ -19,15 +19,7 @@ const ContactSection = () => {
                 style={{ color: "transparent" }}
                 src="images/Simplification-2.svg"
               />
-            </div>
-            <h3 className="mb-2 lg:mt-6 text-h3 font-bold">Rodenkirchen</h3>
-            <div className="mb-4 lg:mb-8 text-p space-y-4">
-              <p>
-                Gustav-Radbruch-Straße 1
-                <br />  
-                50996 Köln
-              </p>
-            </div>
+            </div> */}
             <div className="text-p space-x-4 flex justify-center items-center">
               <div>
                 <Link
@@ -81,7 +73,7 @@ const ContactSection = () => {
                   className="hover:text-yellow transition-colors"
                   aria-label="kontakt@psycho-therapie-koeln.de"
                   target="_self"
-                  href="mailto:kontakt@psycho-therapie-koeln.de"
+                  href="/"
                 >
                   <span className="w-[60px] sm:w-[80px] h-[60px] sm:h-[80px] bg-yellow rounded-[10px] flex justify-center items-center *:sm:w-[42px] *:w-[32px]">
                     <Image
@@ -93,15 +85,23 @@ const ContactSection = () => {
                       decoding="async"
                       data-nimg="1"
                       style={{ color: "transparent" }}
-                      src="images/mail-prax.svg"
+                      src="images/Simplification-2.svg"
                     />
                   </span>
                 </Link>
               </div>
             </div>
+            <h3 className="mb-2 lg:mt-6 text-h3 font-bold">Rodenkirchen</h3>
+            <div className="mb-4 lg:mb-8 text-p space-y-4">
+              <p>
+                Gustav-Radbruch-Straße 1
+                <br />
+                50996 Köln
+              </p>
+            </div>
           </div>
           <div className="lg:w-[calc(50%-32px)] 2xl:w-[calc(33.33%-42.67px)] p-4 sm:p-6 sm:rounded-3xl bg-white shadow-custom_shdow2 hover:shadow-2xl transition-all">
-            <div className="scale-75 lg:scale-100 size-[100px] rounded-[10px] grid place-items-center bg-yellow mx-auto shadow-[0_4px_18px_0_rgba(0,0,0,.14)]">
+            {/* <div className="scale-75 lg:scale-100 size-[100px] rounded-[10px] grid place-items-center bg-yellow mx-auto shadow-[0_4px_18px_0_rgba(0,0,0,.14)]">
               <Image
                 role="img"
                 alt="save-health"
@@ -113,16 +113,7 @@ const ContactSection = () => {
                 style={{ color: "transparent" }}
                 src="images/Simplification-2.svg"
               />
-            </div>
-            <h3 className="mb-2 lg:mt-6 text-h3 font-bold">Südstadt</h3>
-            <div className="mb-4 lg:mb-8 text-p space-y-4">
-              <p>
-                Rolandstraße 55
-                <br />
-                50677 Köln
-              </p>
-            </div>
-            {/* new */}
+            </div> */}
             <div className="text-p space-x-4 flex justify-center items-center">
               <div>
                 <Link
@@ -176,7 +167,7 @@ const ContactSection = () => {
                   className="hover:text-yellow transition-colors"
                   aria-label="kontakt@psycho-therapie-koeln.de"
                   target="_self"
-                  href="mailto:kontakt@psycho-therapie-koeln.de"
+                  href="/"
                 >
                   <span className="w-[60px] sm:w-[80px] h-[60px] sm:h-[80px] bg-yellow rounded-[10px] flex justify-center items-center *:sm:w-[42px] *:w-[32px]">
                     <Image
@@ -188,16 +179,25 @@ const ContactSection = () => {
                       decoding="async"
                       data-nimg="1"
                       style={{ color: "transparent" }}
-                      src="images/mail-prax.svg"
+                      src="images/Simplification-2.svg"
                     />
                   </span>
                 </Link>
               </div>
             </div>
+            <h3 className="mb-2 lg:mt-6 text-h3 font-bold">Südstadt</h3>
+            <div className="mb-4 lg:mb-8 text-p space-y-4">
+              <p>
+                Rolandstraße 55
+                <br />
+                50677 Köln
+              </p>
+            </div>
+            {/* new */}
             {/*  */}
           </div>
           <div className="lg:w-[calc(50%-32px)] 2xl:w-[calc(33.33%-42.67px)] p-4 sm:p-6 sm:rounded-3xl bg-white shadow-custom_shdow2 hover:shadow-2xl transition-all">
-            <div className="scale-75 lg:scale-100 size-[100px] rounded-[10px] grid place-items-center bg-yellow mx-auto shadow-[0_4px_18px_0_rgba(0,0,0,.14)]">
+            {/* <div className="scale-75 lg:scale-100 size-[100px] rounded-[10px] grid place-items-center bg-yellow mx-auto shadow-[0_4px_18px_0_rgba(0,0,0,.14)]">
               <Image
                 role="img"
                 alt="body-icon"
@@ -209,16 +209,7 @@ const ContactSection = () => {
                 style={{ color: "transparent" }}
                 src="images/Simplification-2.svg"
               />
-            </div>
-            <h3 className="mb-2 lg:mt-6 text-h3 font-bold">Hürth</h3>
-            <div className="mb-4 lg:mb-8 text-p space-y-4">
-              <p>
-                Krankenhausstraße 107
-                <br />
-                50354 Hürth
-              </p>
-            </div>
-            {/*  */}
+            </div> */}
             <div className="text-p space-x-4 flex justify-center items-center">
               <div>
                 <Link
@@ -270,7 +261,7 @@ const ContactSection = () => {
                 <Link
                   role="link"
                   className="hover:text-yellow transition-colors"
-                  href="mailto:kontakt@psycho-therapie-huerth.de"
+                  href="/"
                   aria-label="kontakt@psycho-therapie-huerth.de"
                   target="_self"
                 >
@@ -284,12 +275,21 @@ const ContactSection = () => {
                       decoding="async"
                       data-nimg="1"
                       style={{ color: "transparent" }}
-                      src="images/mail-prax.svg"
+                      src="images/Simplification-2.svg"
                     />
                   </span>
                 </Link>
               </div>
             </div>
+            <h3 className="mb-2 lg:mt-6 text-h3 font-bold">Hürth</h3>
+            <div className="mb-4 lg:mb-8 text-p space-y-4">
+              <p>
+                Krankenhausstraße 107
+                <br />
+                50354 Hürth
+              </p>
+            </div>
+            {/*  */}
             {/*  */}
           </div>
         </div>
