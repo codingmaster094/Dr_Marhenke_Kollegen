591a3305699717ba1cf29b96dd40494adfbb2078
dipak-trio-spire <dipak@trio-spire.com>
2025-07-30 18:02:39 +0530

    add impresum page data

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -146,14 +146,19 @@ const Footer = ({Footerdata , OptionDatas , Footer1 , Footer2}) => {
               <p>{OptionDatas.footer_rechtliches_title}</p>
             </div>
             <ul className="space-y-5">
-              {Footer2 && 
+              <li key={i}>
+                <Link role="link" className="link" href={"/Impressum"}>
+                  Impressum
+                </Link>
+              </li>
+              {/* {Footer2 && 
               Footer2?.map((item, i) => (
                 <li key={i}>
                   <Link role="link" className="link" href={item.url}>
                     {item.title}
                   </Link>
                 </li>
-              ))}
+              ))} */}
             </ul>
           </div>
           <div className="space-y-4 shrink-0">
