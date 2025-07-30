415b4c2b1b511203787ced67313aa83eae0cbae3
dipak-trio-spire <dipak@trio-spire.com>
2025-07-30 17:27:48 +0530

    add impresum page data

--- src/app/Impressum/page.js
+++ src/app/Impressum/page.js
@@ -1,17 +1,20 @@
 import React from "react";
 import POST_GET from "../utils/PostsGet";
+
 const page = async () => {
   let impressumData;
   try {
     impressumData = await POST_GET("/page-data/impressum");
+    console.log("Fetched Impressum Data:", impressumData);
   } catch (error) {
     console.error("Error fetching data:", error);
     return <div>Error loading data.</div>;
   }
 
-  if (!impressumData) {
+  if (!impressumData || !impressumData.title || !impressumData.content) {
     return <div>No data available.</div>;
   }
+
   return (
     <section className="sec-page-content">
       <div className="container">
@@ -19,9 +22,7 @@ const page = async () => {
           className="wp-block-heading"
           dangerouslySetInnerHTML={{ __html: impressumData.title }}
         ></h2>
-        <div
-          dangerouslySetInnerHTML={{ __html: impressumData.content }}
-        ></div>
+        <div dangerouslySetInnerHTML={{ __html: impressumData.content }}></div>
       </div>
     </section>
   );
